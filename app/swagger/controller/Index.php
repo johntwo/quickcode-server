e6197f7f53256cb05dba1dab2d5d8fee37aaae4f
zhangkai <920062039@qq.com>
2021-06-05 15:00:08 +0800

    add auto new

--- app/swagger/controller/Index.php
+++ app/swagger/controller/Index.php
@@ -1,25 +1,23 @@
 <?php
 namespace app\swagger\controller;
 
-use app\BaseController;
-
-class Index extends BaseController
+class Index extends ControllerBase
 {
     /**
      * swagger  模块配置信息
      */
     public function config(){
-        return json((new \app\swagger\logic\Index())->config());
+        return json($this->logicIndex->config());
     }
 
     /**
      * swagger  模块api信息
      */
     public function api(){
-        return json((new \app\swagger\logic\Index())->api($this->request->param()));
+        return json($this->logicIndex->api($this->request->param()));
     }
 
     public function annotationToResources(){
-        return (new \app\swagger\logic\Index())->updateResourcesByAnnotation();
+        return $this->logicIndex->updateResourcesByAnnotation();
     }
 }
