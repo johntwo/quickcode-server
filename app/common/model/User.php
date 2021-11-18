302457d89a1a13833c0a0be9fd653224e10a9ed6
张凯 <920062039@qq.com>
2021-11-18 17:48:56 +0800

    增加用户实体类

--- app/common/model/User.php
+++ app/common/model/User.php
@@ -2,7 +2,6 @@
 
 namespace app\common\model;
 
-use app\common\pluings\service\Oss;
 use think\model\concern\SoftDelete;
 
 /**
@@ -13,31 +12,4 @@ class User extends ModelBase
 {
     use SoftDelete;
     protected $hidden = ['password','wxopenid','wxunionid','delete_time'];
-
-    /**
-     * 新增用户
-     */
-    public function add($data){
-        if(!empty($data['password'])){
-            $data['password'] = password_hash($data['password'],1);
-        }
-        $this->save($data);
-    }
-
-    /**
-     * 属性修改器
-     */
-    public function setAvatorAttr($value)
-    {
-        return Oss::convertToDatabaseData($value);
-    }
-
-    /**
-     * 属性获取器
-     */
-    public function getAvatorAttr($value)
-    {
-        return Oss::convertToEntityData($value);
-    }
-
 }
