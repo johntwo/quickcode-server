80e7f514907c53b63a4de336c63c9307e3938a0c
张凯 <920062039@qq.com>
2021-11-18 16:14:21 +0800

    修改数据库迁移代码

---
+++ database/seeds/InitUserData.php
@@ -0,0 +1,32 @@
+<?php
+
+use think\migration\Seeder;
+
+class InitUserData extends Seeder
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $table = $this->table('user');
+        $table->insert([
+            [
+                'id'=>1,
+                'uuid'=>'b71eb590478511ecb73f84a93842bd06',
+                'creator'=>1,
+                'updater'=>1,
+                'username'=>'admin',
+                'nickname'=>'超级管理员',
+                'password'=>'',
+                'create_time'=>'2021-11-17 17:06:56',
+                'update_time'=>'2021-11-17 17:06:56'
+            ]
+        ])->save();
+    }
+}
