95bce653e58028df4fb6e8cf2bb15b6f09f80282
张凯 <920062039@qq.com>
2021-12-08 16:07:51 +0800

    登录报错规范统一

--- app/admin/logic/User.php
+++ app/admin/logic/User.php
@@ -13,14 +13,14 @@ class User extends LogicBase
      *  登录管理端
      */
     public function login($data){
-        assert(!empty($data['username']),'用户名不能为空');
-        assert(!empty($data['password']),'密码不能为空');
+        empty($data['username']) && exception('用户名不能为空');
+        empty($data['password']) && exception('密码不能为空');
 
         $user = \app\common\model\User::where('username',$data['username'])->find();
-        assert(!empty($user),'用户不存在');
+        !empty($user) && exception('用户不存在');
 
         $commonLogicUser = new \app\common\logic\User();
-        assert($commonLogicUser->verifyPassword($user['id'],$data['password']),'用户密码不正确');
+        $commonLogicUser->verifyPassword($user['id'],$data['password']) && exception('用户密码不正确');
 
         $token = Auth::buildToken($user['id']);
         $loginCacheInfo = [
