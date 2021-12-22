7891f4d351073cd2bae5e114a7ff2dd7a7b73e5a
张凯 <920062039@qq.com>
2021-12-22 11:09:55 +0800

    用户未登录，增加状态判断

--- app/admin/middleware/AuthToken.php
+++ app/admin/middleware/AuthToken.php
@@ -3,6 +3,7 @@
 namespace app\admin\middleware;
 
 use app\common\config\cache\CacheKey;
+use app\common\config\result\CodeBase;
 
 class AuthToken
 {
@@ -10,16 +11,16 @@ class AuthToken
     {
         $token = $request->header('token');
         if(empty($token)){
-            \exception("当前未登录，请登录");
+            \exception(CodeBase::$UnLogin['message'],CodeBase::$UnLogin['code']);
         }else{
             $decodeToken = base64_decode($token);
             $decodeToken = explode(":",$decodeToken);
             $userInfo = cache(CacheKey::loginAdminToken()->key([$decodeToken[0]]));
             if(empty($userInfo)){
-                \exception("当前未登录，请登录");
+                \exception(CodeBase::$UnLogin['message'],CodeBase::$UnLogin['code']);
             }else{
                 if(!($userInfo->token_uuid == $decodeToken[1] && $userInfo->id == $decodeToken[0])){
-                    \exception("当前未登录，请登录");
+                    \exception(CodeBase::$UnLogin['message'],CodeBase::$UnLogin['code']);
                 }
             }
             Auth::$CurrentUser = $userInfo;
