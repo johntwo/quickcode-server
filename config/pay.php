351f79ffa111823d76ac939e36d8f0d121249844
张凯 <920062039@qq.com>
2022-01-10 15:11:15 +0800

    小程序 缓存 oss 支付 配置移动到环境变量中

--- config/pay.php
+++ config/pay.php
@@ -15,15 +15,15 @@ return [
             // 类型
             'type'=>'Wechat',
             // 小程序 app_id
-            'app_id' => '',
+            'app_id' => env('pay.drivers-wechat-app_id', ''),
             // 商户id
-            'mch_id' => '',
+            'mch_id' => env('pay.drivers-wechat-mch_id', ''),
             // 商户 API 秘钥
-            'key' => '',
+            'key' => env('pay.drivers-wechat-key', ''),
             // 商户 API 证书， apiclient_cert.pem 的绝对路径
-            'cert_path' => '',
+            'cert_path' => env('pay.drivers-wechat-cert_path', ''),
             // 商户 API 证书， apiclient_key.pem 的绝对路径
-            'key_path' => ''
+            'key_path' => env('pay.drivers-wechat-key_path', ''),
         ]
     ],
 ];
