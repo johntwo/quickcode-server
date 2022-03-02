088d0c5708a389e54a1e4c09752bd3f09e3dee81
张凯 <920062039@qq.com>
2022-03-02 11:14:43 +0800

    用户权限相关

--- app/admin/route/app.php
+++ app/admin/route/app.php
@@ -18,18 +18,18 @@ Route::group(function(){
     // 权限
     Route::get('auth/getTree', '/admin/Auth/getTree');
     // 角色操作
-    Route::get('role/getList', '/admin/Role/getList');
+    Route::get('role/getList', '/admin/Role/getList')->middleware(\app\admin\middleware\ApiPermission::class,['role-list']);
     Route::get('role/getOption', '/admin/Role/getOption');
-    Route::post('role/add', '/admin/Role/add');
-    Route::patch('role/update', '/admin/Role/update');
-    Route::delete('role/del', '/admin/Role/del');
+    Route::post('role/add', '/admin/Role/add')->middleware(\app\admin\middleware\ApiPermission::class,['role-add']);
+    Route::patch('role/update', '/admin/Role/update')->middleware(\app\admin\middleware\ApiPermission::class,['role-update']);
+    Route::delete('role/del', '/admin/Role/del')->middleware(\app\admin\middleware\ApiPermission::class,['role-delete']);
     // 用户
     Route::get('User/current', '/admin/User/current');
     Route::delete('User/logout', '/admin/User/logout');
-    Route::get('user/getList', '/admin/User/getList');
-    Route::post('user/add', '/admin/User/add');
-    Route::patch('user/update', '/admin/User/update');
-    Route::delete('user/del', '/admin/User/del');
+    Route::get('user/getList', '/admin/User/getList')->middleware(\app\admin\middleware\ApiPermission::class,['user-list']);
+    Route::post('user/add', '/admin/User/add')->middleware(\app\admin\middleware\ApiPermission::class,['user-add']);
+    Route::patch('user/update', '/admin/User/update')->middleware(\app\admin\middleware\ApiPermission::class,['user-update']);
+    Route::delete('user/del', '/admin/User/del')->middleware(\app\admin\middleware\ApiPermission::class,['user-delete']);
     // oss
     Route::get('oss/getOssToken', '/admin/oss/getOssToken');
 })->middleware(\app\admin\middleware\AuthToken::class);
